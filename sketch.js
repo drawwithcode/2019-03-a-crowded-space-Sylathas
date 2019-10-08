f365eed56d45cb5bc0fcf049325cd04001f52efd
Sylathas <37075162+Sylathas@users.noreply.github.com>
2019-10-08 20:55:51 +0200

    Gioco Finito

--- sketch.js
+++ sketch.js
@@ -1,70 +1,101 @@
 var keys = [];
 var score = 0;
+var door;
+var numberkeys = 5;
 
 function preload(){
-  // put preload code here
+  //load images
+  key = loadImage('assets/key.png');
+  doorimg = loadImage('assets/door.png');
+  bianco = loadImage('assets/bianco.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  NuovoLivello();
+  background(90,90,90);
+  //create level
+  NuovoLivello(numberkeys);
 }
 
 function draw() {
-  push();
-  texture(floor);
-  rect(0,0, windowWidth, windowHeight);
-  pop();
-
-  for (var i = 0; i < keys.length; i++) {
+  background(90,90,90);
+    var missingkeys;
+    for (var i = 0; i < keys.length; i++) {
         keys[i].display();
     }
+    fill("black");
+    rect(0, 0, windowWidth, windowHeight/6);
+    door.display();
+    push();
+    fill('white');
+    textSize(20);
+    missingkeys =  numberkeys - score;
+    text('chiavi mancanti =' , windowWidth/2 - 100, windowHeight - 10);
+    text(missingkeys ,  windowWidth/2 + 60, windowHeight - 10);
+    pop();
+    push();
+    fill('white');
+    textSize(20);
+    text('clicca le chiavi per prenderle e poi clicca la porta per aprirla', 10, windowHeight - 10);
+    pop();
 }
 
 function mousePressed() {
   for (var i = 0; i < keys.length; i++) {
     keys[i].clicked();
   }
+  door.clicked();
 }
 
-function NuovoLivello(){
-  var lvlkeys = 0;
-  if(score === 0){
-    lvlkeys = 5;
-  } else if(score === 5){
-    lvlkeys = 10;
-  } else if(score === 15){
-    lvlkeys = 30;
-  }
-
+function NuovoLivello(lvlkeys){
+  background(90,90,90);
   for (var i = 0; i < lvlkeys; i++) {
     keys[i] = new Keys();
   }
+  door = new Door();
 }
 
 function Keys() {
-  var a = 40;
-  this.colorbody = color("black");
+  var a = 60;
 
   //define parameters of the keys
-  this.x = random(a, windowWidth - a);
-  this.y = random(windowHeight/6, windowHeight - a);
+  this.x = random(a, windowWidth - a*2);
+  this.y = random(windowHeight/6, windowHeight - a*2);
   this.larghezza = a;
+  this.chiave = key;
 
   //define display of the keys
   this.display = function() {
-    noStroke();
-    fill(this.colorbody);
-    ellipse(this.x, this.y, this.larghezza, this.larghezza); //body
+    push();
+    translate(-this.larghezza/2, -this.larghezza/2);
+    image(this.chiave, this.x, this.y, this.larghezza, this.larghezza);
+    pop();
   }
 
   //define what happens when you click on a key
   this.clicked = function() {
     var d = dist(mouseX, mouseY, this.x, this.y)
     if (d < a/2) {
-      this.colorbody = color(0, 0);
+      this.chiave = bianco;
       score++;
+    }
+  }
+}
+
+function Door() {
+  this.porta = doorimg;
+  //define display of the door
+  this.display = function() {
+    image(this.porta, windowWidth/2 - windowHeight/12, 0, windowHeight/6, windowHeight/6);
+  }
+
+  //define what happens when you click on a key
+  this.clicked = function() {
+    if(mouseX > windowWidth/2 - 65 && mouseX < windowWidth/2 + 65 && mouseY > 0 && mouseY < windowHeight/6 && score >= numberkeys) {
+      numberkeys = numberkeys *2;
+      score = 0;
+      print("ciao");
+      NuovoLivello(numberkeys);
     }
   }
 }
